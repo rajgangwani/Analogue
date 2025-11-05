66df6156951e3a15850f121d5bf255b9efb54bf0
Raj Gangwani <rajgangwani0056@gmail.com>
2025-11-06 00:25:34 +0530

    Add 1

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,15 @@
-# core/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portal.urls')),
 ]
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files (only for development or temporary use on Render)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
